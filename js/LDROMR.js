def7a889f29040df7fa6173be1411a51820ba262
Lasse Deleuran <lassedeleuran@gmail.com>
2019-06-10 23:23:01 +0200

    Added OMR checks for license.

--- js/LDROMR.js
+++ js/LDROMR.js
@@ -31,5 +31,18 @@ LDR.OMR.FixAuthors = function(expectedAuthor) {
         pt.author = expectedAuthor;
     }};
 
-    return {title:title, checkers:checkers};
+    return {title:title, checkers:checkers, handlers:handlers};
+}
+
+LDR.OMR.FixLicenses = function() {
+    const LICENSE = 'Redistributable under CCAL version 2.0 : see CAreadme.txt';
+    let title = "Click here to change all license lines in the models of the LDraw file (including unofficial models, if any) to '" + LICENSE + "'";
+
+    let checkers = {checkPartType: pt => pt.license !== LICENSE};
+    
+    let handlers = {handlePartType: pt => {
+	pt.license = LICENSE;
+    }};
+
+    return {title:title, checkers:checkers, handlers:handlers};
 }
