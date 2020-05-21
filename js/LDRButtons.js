7ea72f15cb30707d28db70766f06235a317030c8
Lasse Deleuran <lassedeleuran@gmail.com>
2020-05-21 13:11:45 +0200

    Improved UI fading to reduce faulty taps on mobile.

--- js/LDRButtons.js
+++ js/LDRButtons.js
@@ -72,9 +72,8 @@ LDR.Buttons = function(actions, element, addTopButtons, homeLink, mainImage, opt
 	$('.ui_control').css('opacity', 1);
         onFadeInComplete();
     };
-    $("#main_canvas, #preview").mousemove(runUIFading);
-    $("canvas, .ui_control").on('tap', runUIFading);
-    $(".ui_control").click(runUIFading);
+    $("#main_canvas, #preview, #next_button_large, #next_button, .ui_control").mousemove(runUIFading);
+    $(".ui_control").on('tap', runUIFading);
 }
 
 LDR.Buttons.prototype.addTopButtonElements = function(actions, element, homeLink, mainImage, options) {
