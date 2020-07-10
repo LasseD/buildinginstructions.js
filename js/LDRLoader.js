9366039fe4be9e2b6eca2615aca9395027e45520
Deleuran, Lasse <lasse.deleuran@adidas.com>
2020-07-10 14:17:53 +0200

    Fixed stud generation issue numbber 41.

--- js/LDRLoader.js
+++ js/LDRLoader.js
@@ -55,17 +55,31 @@ THREE.LDRLoader = function(onLoad, storage, options) {
             return;
         }
         // Try to fetch those that can be generated:
-        let stillToBeFetched = [];
-        toBeFetched.forEach(id => {
-                let pt = LDR.Generator.make(id)
-                if(pt) {
-                    loader.setPartType(pt);
-                }
-                else {
-                    stillToBeFetched.push(id);
-                }
-            });
-        onDone(stillToBeFetched);
+        let improved = true;
+        while(improved) {
+            improved = false;
+            let stillToBeFetched = [];
+            let added = {};
+            toBeFetched.forEach(id => {
+                    if(added.hasOwnProperty(id) || self.partTypes.hasOwnProperty(id)) {
+                        return; // Already seen.
+                    }
+                    added[id] = true;
+
+                    let pt = LDR.Generator.make(id)
+                    if(pt) {
+                        loader.setPartType(pt);
+                        // Check for sub-models:
+                        pt.steps.forEach(step => step.subModels.forEach(sm => stillToBeFetched.push(sm.ID)));
+                        improved = true;
+                    }
+                    else {
+                        stillToBeFetched.push(id);
+                    }
+                });
+            toBeFetched = stillToBeFetched;
+        }
+        onDone(toBeFetched);
     }
     this.storage = storage || {retrievePartsFromStorage:backupRetrievePartsFromStorage};
     this.options = options || {};
